a7209ab8b299202cec5a22615462be8447e12983
Tôn Thất Minh Vũ <ITITIU20356@student.hcmiu.edu.vn>
2021-10-26 16:06:58 +0700

    Ex 4,5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -36,64 +36,19 @@ void Array2Dconverter(int arr[], int a[SIZE][SIZE], int m, int n)
 void Ex5(int arr[], int m, int n){
 	int a[SIZE][SIZE];
 	Array2Dconverter(arr,a,m,n);
-	//Your codes here
 	int max = 0;
-    printf("\n");
-    for(int i=0; i < n; i++)
-    {
-        printf("Minimum element is row %d = %d\n", (i+1), result[i]);
-        
-        if(result[i] > max){
-            max = result[i];
-        }
-    }
-    
-    printf("\nOutput = %d\n", max);
-}
-    int i = 0, j;
-    int min = 99999;
-    int result[];
-    while (i < m){
-        for ( j = 0; j < n; j++)
-        {
-            if (mat[i][j] < min)
-            {
-                min = mat[i][j];
-            }
-        }
-    
-        min = 99999;
-        i++;
-    }
-    
-}
-
-void displayMatrix(int m, int n, int matrix[][n])
-{
-    printf("\nMatrix:\n");
-    for(int i=0; i < m; i++) {
-        for(int j=0; j < n; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int main()
-{
-    int m, n;
-    printf("Input: ");
-    scanf("%d %d",&m,&n);
-    
-    int i, j;
-    int matrix[m][n];
-    for(i = 0; i < m; i++) {
-        for(j = 0; j < n; j++) {
-            scanf_s("%d", &matrix[i][j]);
-        }
-    }
-    
-    return 0;
+	for (int i = 0; i < m; i++)
+	{
+		int min = a[i][0];
+		for (int j = 0; j < n; j++)
+		{
+			if (min > a[i][j])
+				min = a[i][j];
+		}
+		if (max < min)
+			max = min;
+	}
+	printf("%d", max);
 }
 
 int main(int argc, char *argv[]) {
